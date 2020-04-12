60b52d0684dfb490324f5c12cea88cab8bc44a95
KIMINJOONG <ijboym216@gmail.com>
2020-04-12 18:01:36 +0900

    init commit

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+interface IProps {
+    styleTags: Array<React.ReactElement<{}>>;
+}
+
+export default class MyDocument extends Document<IProps> {
+    static getInitialProps(context: any) {
+        const sheet = new ServerStyleSheet();
+        const page = context.renderPage((App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />)
+        );
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>{this.props.styleTags}</Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Row, Col } from "antd";
+import { Fragment } from "react";
+
+const ContainerT = styled.div`
+    border: 1px solid red;
+`;
+
+const Home = () => (
+    <Fragment>
+        <ContainerT>hi</ContainerT>
+    </Fragment>
+);
+
+export default Home;
