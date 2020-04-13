d19822f224534ac33404274d6890650f224a16a6
KIMINJOONG <ijboym216@gmail.com>
2020-04-14 00:00:17 +0900

    apply antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import styled from "styled-components";
-import { Row, Col } from "antd";
-import { Fragment } from "react";
-
-const ContainerT = styled.div`
-    border: 1px solid red;
-`;
+import { Input, Menu } from "antd";
+import Link from "next/link";
 
 const Home = () => (
-    <Fragment>
-        <ContainerT>hi</ContainerT>
-    </Fragment>
+    <div>
+        <Menu mode="horizontal">
+            <Menu.Item key="home">
+                <Link href="/">
+                    <a>리액트버드</a>
+                </Link>
+            </Menu.Item>
+            <Menu.Item key="profile">
+                <Link href="/profile">
+                    <a>프로필</a>
+                </Link>
+            </Menu.Item>
+            <Menu.Item key="mail"></Menu.Item>
+        </Menu>
+    </div>
 );
 
 export default Home;
