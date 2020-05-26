93b0b5d11534f4a270a702446dc75429d926ebac
KIMINJOONG <ijboym216@gmail.com>
2020-05-26 19:30:49 +0900

    mobile header

--- components/AppLayout/index.tsx
+++ components/AppLayout/index.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent, useContext, useCallback } from "react";
-import { Layout, Row, Col, Button, message } from "antd";
+import { FunctionComponent, useContext, useCallback, useState } from "react";
+import { Layout, Row, Col, Button, message, Drawer } from "antd";
 import {
     Header,
     Ul,
@@ -12,78 +12,108 @@ import {
 import Link from "next/link";
 import userStore from "../../stores/userStore";
 import jsCookie from "js-cookie";
+import { MdFormatIndentIncrease, MdFormatIndentDecrease } from "react-icons/md";
 
 const AppLayout: FunctionComponent = ({ children }) => {
     const userState = useContext(userStore);
+    const [visible, setVisible] = useState(false);
 
     const onClickLogout = useCallback(async () => {
         await jsCookie.remove("token");
         userState.getMe();
         message.success("로그아웃 되었습니다");
     }, [userState]);
+
+    const onClose = useCallback(() => {
+        setVisible(false);
+    }, [visible]);
     return (
         <Layout>
             <Row>
-                {/* pc헤더 */}
-                <Header xs={0}>
-                    <Row style={{ height: "100%" }}>
-                        <Navigation span={16}>
-                            <Ul>
-                                <li>
-                                    <Link href="/">
-                                        <a>Home</a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/boards">
-                                        <a>Board</a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/videos">
-                                        <a>Video</a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/about">
-                                        <a>About</a>
-                                    </Link>
-                                </li>
-                            </Ul>
-                            <Logo
-                                style={{
-                                    display: "flex",
-                                    flex: 1,
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <Link href="/">
-                                    <a>
-                                        <h1>Kohubi's Blog</h1>
-                                    </a>
-                                </Link>
-                            </Logo>
-                            <LeftUl>
-                                <li>
-                                    {userState &&
-                                    userState.value &&
-                                    userState.value.id ? (
-                                        <Button
-                                            onClick={onClickLogout}
-                                            type="link"
-                                            ghost
-                                        >
-                                            <a>로그아웃</a>
-                                        </Button>
-                                    ) : (
-                                        <Link href="/login">
-                                            <a>로그인</a>
+                <Header>
+                    <Row justify="center">
+                        {/* pc헤더 */}
+                        <Col xs={0} md={16}>
+                            <Navigation>
+                                <Ul>
+                                    <li>
+                                        <Link href="/">
+                                            <a>Home</a>
                                         </Link>
-                                    )}
-                                </li>
-                            </LeftUl>
-                        </Navigation>
+                                    </li>
+                                    <li>
+                                        <Link href="/boards">
+                                            <a>Board</a>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/videos">
+                                            <a>Video</a>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/about">
+                                            <a>About</a>
+                                        </Link>
+                                    </li>
+                                </Ul>
+                                <Logo>
+                                    <Link href="/">
+                                        <a>
+                                            <h1>Kohubi's Blog</h1>
+                                        </a>
+                                    </Link>
+                                </Logo>
+                                <LeftUl>
+                                    <li>
+                                        {userState &&
+                                        userState.value &&
+                                        userState.value.id ? (
+                                            <Button
+                                                onClick={onClickLogout}
+                                                type="link"
+                                                ghost
+                                            >
+                                                <a>로그아웃</a>
+                                            </Button>
+                                        ) : (
+                                            <Link href="/login">
+                                                <a>로그인</a>
+                                            </Link>
+                                        )}
+                                    </li>
+                                </LeftUl>
+                            </Navigation>
+                        </Col>
+                        {/* 모바일 헤더 */}
+                        <Col xs={24} md={0}>
+                            {visible ? (
+                                <MdFormatIndentDecrease
+                                    style={{ color: "white" }}
+                                    size={30}
+                                    onClick={() => setVisible(false)}
+                                />
+                            ) : (
+                                <MdFormatIndentIncrease
+                                    style={{ color: "white" }}
+                                    size={30}
+                                    onClick={() => setVisible(true)}
+                                />
+                            )}
+
+                            <Drawer
+                                title="Basic Drawer"
+                                placement={"left"}
+                                closable={false}
+                                onClose={onClose}
+                                visible={visible}
+                                key={"key"}
+                            >
+                                <p>Some contents...</p>
+                                <p>Some contents...</p>
+                                <p>Some contents...</p>
+                            </Drawer>
+                        </Col>
                     </Row>
                 </Header>
             </Row>
