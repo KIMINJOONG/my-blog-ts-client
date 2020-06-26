58fc505e20fa957edb0686ebf2cb078ba1d21e91
KIMINJOONG <ijboym216@gmail.com>
2020-06-26 17:15:55 +0900

    apply reactHtmlParser

--- pages/boards/[id].tsx
+++ pages/boards/[id].tsx
@@ -2,26 +2,31 @@ import { useRouter } from "next/router";
 import Edit from "../../components/Edit";
 import { useState, useEffect, useCallback } from "react";
 import api from "../../api";
+import ReactHtmlParser from "react-html-parser";
 
+interface board {
+    content: string;
+    createdAt: string;
+    id: number;
+    title: string;
+    updatedAt: string;
+    userId: number;
+}
 const edit = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [data, setData] = useState(null);
+    const router = useRouter();
+    const { id } = router.query;
+    const [data, setData] = useState({} as board);
 
-  const init = useCallback(async () => {
-    const result = await api.show(`/boards/${id}`);
-    const { data, status: httpStatus } = result;
-    setData(data.data);
-  }, []);
-  useEffect(() => {
-    init();
-  }, []);
+    const init = useCallback(async () => {
+        const result = await api.show(`/boards/${id}`);
+        const { data, status: httpStatus } = result;
+        setData(data.data);
+    }, []);
+    useEffect(() => {
+        init();
+    }, []);
 
-  return (
-    <div>
-      <Edit param={id} data={data} preset={"inline"} disabled={true} />
-    </div>
-  );
+    return <div>{data && ReactHtmlParser(data.content)}</div>;
 };
 
 export default edit;
