d6f0bfaa65378c40766b79001afeaa4160b1686d
KIMINJOONG <ijboym216@gmail.com>
2020-09-06 19:06:20 +0900

    change api url

--- sagas/index.ts
+++ sagas/index.ts
@@ -3,10 +3,11 @@ import user from "./user";
 import board from "./board";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "production"
-  ? "http://54.150.124.198"
-  : "http://localhost:4000";
+axios.defaults.baseURL =
+    process.env.NODE_ENV === "production"
+        ? "https://api.kohubi.xyz"
+        : "http://localhost:4000";
 
 export default function* rootSaga() {
-  yield all([call(user), call(board)]);
+    yield all([call(user), call(board)]);
 }
