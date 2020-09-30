6e29686bc4e911a2e64b097fd1510b86d8712bc2
KIMINJOONG <ijboym216@gmail.com>
2020-09-30 22:42:19 +0900

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,28 +40,28 @@ export default class MyDocument extends Document<IProps> {
           {this.props.styleTags}
         </Head>
         <NextHead>
-          {/* <meta
+          <meta
             name="viewport"
             content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
           />
           <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
-          <meta name="subject" content="Kohubi's Blog" />
-          <meta name="title" content="Kohubi's Blog" />
-          <meta name="author" content="김인중 / kohubi" />
+          <meta name="subject" content="코후비 블로그" />
+          <meta name="title" content="코후비 블로그" />
+          <meta name="author" content="김인중 | kohubi" />
           <meta name="keywords" content="비보이,개발,개발자" />
           <meta
             name="description"
             content="개발자 김인중이자 비보이 kohubi의 개발과 일상을 담은 블로그입니다."
-          /> */}
+          />
 
           {/* Open Graph */}
           <meta property="og:url" content="https://kohubi.xyz" />
           <meta property="og:title" content="코후비 블로그" />
-          <meta property="og:type" content="website" />
           <meta
             property="og:image"
-            content="http://www.bbsetheme.com/wp-content/uploads/2017/11/bbsetheme_logo.png"
+            content="https://kohubi.xyz/logo.png"
           />
           <meta property="og:description" content="코후비 블로그" />
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,6 @@ import {
 } from "../reducers/board";
 import ReactHtmlParser from "react-html-parser";
 import { useSelector } from "react-redux";
-import Head from "next/head";
 
 interface ICategory {
   id: string;
@@ -121,47 +120,6 @@ const Home: NextPage = () => {
   };
   return (
     <AppLayout>
-      <Head>
-        {/* <meta
-            name="viewport"
-            content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
-          />
-          <meta charSet="utf-8" />
-          <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
-          <meta name="subject" content="Kohubi's Blog" />
-          <meta name="title" content="Kohubi's Blog" />
-          <meta name="author" content="김인중 / kohubi" />
-          <meta name="keywords" content="비보이,개발,개발자" />
-          <meta
-            name="description"
-            content="개발자 김인중이자 비보이 kohubi의 개발과 일상을 담은 블로그입니다."
-          /> */}
-
-        {/* Open Graph */}
-        <meta property="og:url" content="https://kohubi.xyz" />
-        <meta property="og:title" content="코후비 블로그" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="http://www.bbsetheme.com/wp-content/uploads/2017/11/bbsetheme_logo.png"
-        />
-        <meta property="og:description" content="코후비 블로그" />
-
-        {/* <link rel="canonical" href="https://kohubi.xyz" /> */}
-        <link
-          rel="shortcut icon"
-          href="https://kohubi.xyz/favicon.ico"
-          type="image/x-icon"
-        />
-        <meta
-          name="naver-site-verification"
-          content="b859482b7c9f0278e01a56189ba14a7b55f7489d"
-        />
-        <meta
-          name="google-site-verification"
-          content="O4gmbMWol2odBM0qvx_2cY02Ilbp_3l-Px69viCH2Ng"
-        />
-      </Head>
       <Row>
         <Col xs={0} md={24}>
           <h2>LATEST</h2>
