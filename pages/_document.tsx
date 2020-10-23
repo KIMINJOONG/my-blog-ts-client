828510b722a7cf40a722149310d685fc33788150
KIMINJOONG <ijboym216@gmail.com>
2020-10-23 12:59:56 +0900

    chaneg meta tag
    1. og tag chaneg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -57,13 +57,11 @@ export default class MyDocument extends Document<IProps> {
           />
 
           {/* Open Graph */}
-          <meta property="og:url" content="https://kohubi.xyz" />
+          <meta property="og:type" content="website" /> 
           <meta property="og:title" content="코후비 블로그" />
-          <meta
-            property="og:image"
-            content="https://kohubi.xyz/logo.png"
-          />
-          <meta property="og:description" content="코후비 블로그" />
+          <meta property="og:description" content="춤추는 개발자 코후비 블로그" />
+          <meta property="og:image" content="https://kohubi.xyz/logo.png" />
+          <meta property="og:url" content="https://kohubi.xyz" />
 
           {/* <link rel="canonical" href="https://kohubi.xyz" /> */}
           <link
