27eafbea1123dc4b7cb445aa8e7b3ea40953dbf4
KIMINJOONG <ijboym216@gmail.com>
2021-06-16 10:17:57 +0900

    tag test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -64,15 +64,6 @@ const MyBlog: any = ({ Component }: any) => {
         <meta property="og:title" content="My page title" key="title" />
       </Head>
       <Head>
-        <meta property="og:title" content="My new title" key="title" />
-        <meta
-          property="og:image"
-          content="https://image.shutterstock.com/image-vector/sample-stamp-grunge-texture-vector-260nw-1389188327.jpg"
-        />
-        <meta property="og:description" content="Og tag test!!!" />
-        <meta property="og:title" content="Og tag test!!! title" />
-      </Head>
-      <Head>
         {/* <link rel="canonical" href="https://kohubi.xyz" /> */}
         <link
           rel="shortcut icon"
