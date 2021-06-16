72905e315f88605e416001e1ca26bb7eab1de00c
KIMINJOONG <ijboym216@gmail.com>
2021-06-16 12:28:48 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,7 @@ import { NextPage } from "next";
 import AppLayout from "../components/AppLayout";
 
 const Home: NextPage = () => {
-  return (
-    <AppLayout>
-      <div>테스트</div>
-    </AppLayout>
-  );
+  return <div>테스트</div>;
 };
 
 export default Home;
