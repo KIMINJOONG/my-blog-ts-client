c1e7f9e1485ba49dd59702eddba449c026d4eaae
KIMINJOONG <ijboym216@gmail.com>
2021-06-16 12:49:59 +0900

    Test

--- components/AppLayout/index.tsx
+++ components/AppLayout/index.tsx
@@ -12,6 +12,7 @@ import { LOAD_COUNT_BY_TODAY_REQUEST } from "../../reducers/board";
 import { RootState } from "../../reducers";
 import { IHashtag } from "../../types/hashtag";
 import { ICategory } from "../../types/category";
+import Head from "next/head";
 
 const AppLayout: FunctionComponent = ({ children }) => {
   const { countByToday } = useSelector((state: RootState) => state.board);
@@ -62,6 +63,46 @@ const AppLayout: FunctionComponent = ({ children }) => {
 
   return (
     <div>
+      <Head>
+        <title>Kohubi's Blog</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+        />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="subject" content="코후비 블로그" />
+        <meta name="title" content="코후비 블로그" />
+        <meta name="author" content="김인중 | kohubi" />
+        <meta name="keywords" content="비보이,개발,개발자" />
+        <meta name="description" content="춤추는 개발자 코후비 블로그" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="코후비 블로그" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kohubi.xyz" />
+        <meta property="og:description" content="춤추는 개발자 코후비 블로그" />
+        <meta property="og:image" content="https://kohubi.xyz/logo.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="800" />
+
+        {/* <link rel="canonical" href="https://kohubi.xyz" /> */}
+        <link
+          rel="shortcut icon"
+          href="https://kohubi.xyz/favicon.ico"
+          type="image/x-icon"
+        />
+        <meta
+          name="naver-site-verification"
+          content="b859482b7c9f0278e01a56189ba14a7b55f7489d"
+        />
+        <meta
+          name="google-site-verification"
+          content="O4gmbMWol2odBM0qvx_2cY02Ilbp_3l-Px69viCH2Ng"
+        />
+      </Head>
       <Row justify="center" style={{ backgroundColor: "#fafafa" }}>
         <Col xs={24} md={20}>
           <Row>
